387a56c2b37dd6d121d973212c53d681a27f6b01
DiEvil21 <55885322+DiEvil21@users.noreply.github.com>
2023-11-30 15:25:24 +0300

    Logout complete

--- app/src/main/java/ru/dievil/aeonapp/View/LoginActivity.kt
+++ app/src/main/java/ru/dievil/aeonapp/View/LoginActivity.kt
@@ -1,5 +1,6 @@
 package ru.dievil.aeonapp.View
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -22,14 +23,33 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var editTextPassword: EditText
     private lateinit var loginButton: Button
 
+    private val sharedPreferences by lazy {
+        getSharedPreferences("login_preferences", Context.MODE_PRIVATE)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
         // Инициализация
         viewModel = ViewModelProvider(this).get(AuthViewModel::class.java)
         editTextUsername = findViewById(R.id.et_login)
         editTextPassword = findViewById(R.id.et_password)
         loginButton = findViewById(R.id.btn_login)
+
+        // Попытка автоматического входа
+        if (isLoggedIn()) {
+            val savedUsername = sharedPreferences.getString("username", "")
+            val savedPassword = sharedPreferences.getString("password", "")
+
+            editTextUsername.setText(savedUsername)
+            editTextPassword.setText(savedPassword)
+
+            // Выполняем вход
+            if (savedUsername!= null && savedPassword != null) {
+                login(savedUsername, savedPassword)
+            }
+        }
 
         // Слушатель на кнопку входа
         loginButton.setOnClickListener {
@@ -38,26 +58,45 @@ class LoginActivity : AppCompatActivity() {
 
             // Проверка ввели ли данные
             if (username.isNotEmpty() && password.isNotEmpty()) {
-                val loginRequest = LoginRequest(username, password)
-                Log.d("retrofit", loginRequest.toString())
+                // Сохраняем логин и пароль
+                saveCredentials(username, password)
+
                 // Вызов метода для входа
-                viewModel.login(loginRequest).observe(this) { token ->
-                    if (token.isNotEmpty()) {
-                        Log.d("retrofit", token)
-                        val intent = Intent(this, PaymentsActivity::class.java)
-                        // Передаем токен в PaymentsActivity
-                        intent.putExtra("TOKEN", token)
-                        startActivity(intent)
-                    } else {
-                        // Неудачный вход
-                        Toast.makeText(this, "Неудачный вход", Toast.LENGTH_SHORT).show()
-                    }
-                }
+                login(username, password)
             } else {
                 // Пустые данные
                 Toast.makeText(this, "Введите логин и пароль", Toast.LENGTH_SHORT).show()
             }
         }
     }
+
+    private fun isLoggedIn(): Boolean {
+        return sharedPreferences.getBoolean("isLoggedIn", false)
+    }
+
+    private fun saveCredentials(username: String, password: String) {
+        val editor = sharedPreferences.edit()
+        editor.putString("username", username)
+        editor.putString("password", password)
+        editor.putBoolean("isLoggedIn", true)
+        editor.apply()
+    }
+
+    private fun login(username: String, password: String) {
+        val loginRequest = LoginRequest(username, password)
+        viewModel.login(loginRequest).observe(this) { token ->
+            if (token.isNotEmpty()) {
+                Log.d("retrofit", token)
+                val intent = Intent(this, PaymentsActivity::class.java)
+                // Передаем токен в PaymentsActivity
+                intent.putExtra("TOKEN", token)
+                startActivity(intent)
+            } else {
+                // Неудачный вход
+                Toast.makeText(this, "Неудачный вход", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
+
 
--- app/src/main/java/ru/dievil/aeonapp/View/PaymentsActivity.kt
+++ app/src/main/java/ru/dievil/aeonapp/View/PaymentsActivity.kt
@@ -1,5 +1,7 @@
 package ru.dievil.aeonapp.View
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,6 +9,7 @@ import ru.dievil.aeonapp.R
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import ru.dievil.aeonapp.ViewModel.AuthViewModel
 
 class PaymentsActivity : AppCompatActivity() {
@@ -14,6 +17,7 @@ class PaymentsActivity : AppCompatActivity() {
     private lateinit var viewModel: AuthViewModel
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: PaymentsAdapter
+    private lateinit var fabLogout: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +40,23 @@ class PaymentsActivity : AppCompatActivity() {
                 recyclerView.adapter = adapter
             }
         }
+        fabLogout = findViewById(R.id.fabLogout)
+        fabLogout.setOnClickListener {
+            logout()
+        }
+    }
+    private fun logout() {
+        clearCredentials()
+        val intent = Intent(this, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+    }
+
+    private fun clearCredentials() {
+        val sharedPreferences = getSharedPreferences("login_preferences", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.clear()
+        editor.apply()
     }
 }
 
